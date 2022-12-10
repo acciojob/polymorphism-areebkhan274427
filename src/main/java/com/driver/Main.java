e3ac978dc9e630f0067e9abf7eaab3b67ecb9fd4
Areeb <areebkhan274427@gmail.com>
2022-12-10 16:08:04 +0530

    New Changes

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -6,13 +6,13 @@ public class Main {
 
     public static class Product{
 
-        public int Product(int x,int y){
+        public int product(int x,int y){
             return x*y;
         }
-        public int Product(int x,int y,int z){
+        public int product(int x,int y,int z){
             return x*y*z;
         }
-        public double Product(double x,double y){
+        public double product(double x,double y){
             return x*y;
         }
 
@@ -22,9 +22,9 @@ public class Main {
         Product p=new Product();
 
 
-        p.Product(1,2);
-        p.Product(1,2,3);
-        p.Product(1.2,1.4);
+        p.product(1,2);
+        p.product(1,2,3);
+        p.product(1.2,1.4);
 
 
 
